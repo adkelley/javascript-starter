7f1fc2f09becea6115d54bc9582f86195bd98483
Alex Kelley <adkelley@mac.com>
2019-01-02 12:14:24 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/* eslint-env node */
+
+import printMe from './print.js';
+const {reduce, concat, map} = require ('sanctuary');
+
+function join(list, char) {
+  return reduce (concat) ('') (map (x => concat (x) (char)) (list));
+}
+
+function component() {
+  const element = document.createElement ('div');
+  const btn = document.createElement ('button');
+
+  element.innerHTML = join (['Hello', 'Webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick = printMe;
+
+  element.appendChild (btn);
+
+  return element;
+}
+
+document.body.appendChild (component ());
